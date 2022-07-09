84c04f530a0280c918bdfad5203061c10e634d3e
Jeppe Reinhold <jeppe@reinhold.is>
2022-07-09 22:55:09 +0200

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script lang="ts" context="module">
+	export const load: import('@sveltejs/kit').Load = async ({ session }) => {
+		return {
+			props: {
+				cf: session.cf
+			}
+		};
+	};
+</script>
+
+<script lang="ts">
+	export let cf: string;
+</script>
+
+<h1><code>cf</code> property from request</h1>
+<pre>
+    <code>
+        {cf}
+    </code>
+</pre>
